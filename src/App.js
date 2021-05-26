5b6aada66475a6a69af46bbd3310575bce4f7e21
Rashika_98_Dev <rashikamr1998@gmail.com>
2021-05-26 23:19:36 +0530

    Updated.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello world..!</h1>
-      </header>
-    </div>
+    <>
+      
+    </>
   );
 }
 
