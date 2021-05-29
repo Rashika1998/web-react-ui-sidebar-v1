767e5e9487dabd5d9fa5554bd0581e596a82f40e
Rashika_98_Dev <rashikamr1998@gmail.com>
2021-05-29 20:15:54 +0530

    footer implementation initialized.

---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div id='main-div'>
+            <div id='sub-div'>
+                <div id='sub-sub-div'>
+                    <div><p>Hello world</p></div>
+                    <div><p>Hello world</p></div>
+                    <div><p>Hello world</p></div>
+                    <div><p>Hello world</p></div>
+                </div>
+                <div id='sub-sub-div'>
+                    <div><p>Hello world</p></div>
+                    <div><p>Hello world</p></div>
+                    <div><p>Hello world</p></div>
+                    <div><p>Hello world</p></div>
+                </div>
+                <div id='sub-sub-div'>
+                    <div><p>Hello world</p></div>
+                    <div><p>Hello world</p></div>
+                    <div><p>Hello world</p></div>
+                    <div><p>Hello world</p></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
